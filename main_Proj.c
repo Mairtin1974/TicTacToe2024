ecdde0e96a88067e659f97c5e44f1e7ab88a02d5
Mairtin O Conghaile <mairtin.oconghaile@atu.ie>
2024-02-28 12:37:09 +0000

    Added basic user interface to select X/O location

--- main_Proj.c
+++ main_Proj.c
@@ -5,6 +5,14 @@ int main()
     int A,B;
     char grid[9];
 
+    printf("lets play Xs and Os");
+
+    for (int i=0; i<9;i++)
+    {
+    printf("Please enter the location of your %d X!!", i);
+
+    }
+
 
     return 0;
 }
